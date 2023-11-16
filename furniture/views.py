eba6a9e9ddebb4bd9f5092f8f7417caa5066da03
vaishnav janardhanan <vaishnavjanardhanan22@gmail.com>
2023-11-16 14:14:08 +0530

    admin panel through adding

--- furniture/views.py
+++ furniture/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from .models import furniture
 
 
 # Create your views here.
 def home(request):
-    return render(request, 'index.html')
+    obj = furniture.objects.all()
+    return render(request, 'index.html', {'object': obj})
--- pro_furniture/settings.py
+++ pro_furniture/settings.py
@@ -73,8 +73,11 @@ WSGI_APPLICATION = 'pro_furniture.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+         'NAME': 'furniture_db',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost'
     }
 }
 
@@ -115,6 +118,9 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR,'static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR,'assets')
+
+MEDIA_URL = 'media/'
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
