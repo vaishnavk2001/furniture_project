4c8e091adcd63e115257f5614137d935346c8322
vaishnav janardhanan <vaishnavjanardhanan22@gmail.com>
2023-11-16 19:10:38 +0530

    migrate table

--- furniture/models.py
+++ furniture/models.py
@@ -6,3 +6,6 @@ class furniture(models.Model):
     name = models.CharField(max_length=50)
     price = models.IntegerField()
     img = models.ImageField(upload_to='pictures')
+    material = models.CharField(max_length=50,default=None)
+    desc = models.TextField(default=None)
+
